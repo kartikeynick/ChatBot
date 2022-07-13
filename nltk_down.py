7f774a056fa7908acf8e01172fdac26a4c9a4f88
kartikeynick <47652496+kartikeynick@users.noreply.github.com>
2022-07-13 00:39:24 -0700

    New

--- nltk_down.py
+++ nltk_down.py
@@ -1,29 +1,36 @@
 import nltk
-#nltk.download('punkt') #pretrained tokenizer package
-from nltk.stem.porter import PorterStemmer #for stemmer
+# nltk.download('punkt') #pretrained tokenizer package
+from nltk.stem.porter import PorterStemmer  # for stemmer
 import numpy as np
-stemmer=PorterStemmer() #creater a stemmer
-def tokenize (sentence):
+
+stemmer = PorterStemmer()  # creater a stemmer
+
+
+def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
-def stem(word):
-    return stemmer.stem(word.lower())# return all to the lowercase
 
-def bagOfWords(tokenizedSentence,all_words): #tokenized sentence and all words
+def stem(word):
+    return stemmer.stem(word.lower())  # return all to the lowercase
+
+
+def bagOfWords(tokenizedSentence, all_words):  # tokenized sentence and all words
     '''S=['hello','how','are','you']
     words=['hi','hello','i','you','bye','thank','cool']
 
     bog= [0,1,0,1,0,0,0]
     making bag of words
     '''
-    #Now by using the list comprehension.
+    # Now by using the list comprehension.
     tokenizedSentence = [stem(w) for w in tokenizedSentence]
     print(tokenizedSentence)
-    bag=np.zeros(len(all_words),dtype=np.float32)
+    bag = np.zeros(len(all_words), dtype=np.float32)
     for idx, w in enumerate(all_words):
         if w in tokenizedSentence:
-            bag[idx]=1.0
+            bag[idx] = 1.0
     return bag
+
+
 '''S=['hello','how','are','you']
 
 #S=["organize", "organization","organizing"]
@@ -31,7 +38,6 @@ words=['hi','hello','i','you','bye','thank','cool']
 bog=bagOfWords(S,words)
 print(bog)
 '''
-
 
 '''
 #test the tokenizer
--- train.py
+++ train.py
@@ -1,65 +1,83 @@
 import json
-from nltk_down import tokenize, stem,bagOfWords
+from nltk_down import tokenize, stem, bagOfWords
 import numpy as np
 
 import torch
-import  torch.nn as nn
+import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
-
-
-
-#to load the json file
+# to load the json file
 with open('intents.json', 'r') as f:
-    intents=json.load(f)
+    intents = json.load(f)
 
-#print(intents)
+# print(intents)
 
-allWords=[]# empty list
-tags=[]# for tags
-xy=[]#later holds all words and tags
+allWords = []  # empty list
+tags = []  # for tags
+xy = []  # later holds all words and tags
 
-for intent in intents ['intents']:
-    tag=intent['tag'] #key tag as in the json file
-    tags.append(tag) #then we will be appending it in out tag array and
-    for pattern in intent['patterns']: # loop through every pattern with this tag in the json file
-        tknize=tokenize(pattern)
-        allWords.extend(tknize) #append it into the all word array
-        xy.append((tknize,tag)) # so it will know the pattern and the corrosponding tag
+for intent in intents['intents']:
+    tag = intent['tag']  # key tag as in the json file
+    tags.append(tag)  # then we will be appending it in out tag array and
+    for pattern in intent['patterns']:  # loop through every pattern with this tag in the json file
+        tknize = tokenize(pattern)
+        allWords.extend(tknize)  # append it into the all word array
+        xy.append((tknize, tag))  # so it will know the pattern and the corresponding tag
 
-#now excluding the punchuation characters, kind of removing all the stopwords
+# Now excluding the punctuation characters, Later Implication: Removing all the stopwords later down the road if
+# needed, but for now this works
 
-ignoreWords=['?','!','.',',']
-allWords=[stem(w) for w in allWords if w not in ignoreWords]
-print(allWords) #this will give a tokenized and stemmed word and removed the special characters
-allWords=sorted((set(allWords))) # sort and remove all the duplicate words
-tags=sorted((set(tags)))
+ignoreWords = ['?', '!', '.', ',']  # Array of punctuation characters. We Will not be needing them for our BOW model
+allWords = [stem(w) for w in allWords if w not in ignoreWords]  # do Stemming
+print(allWords)  # This will give a tokenized and stemmed word and removed the special characters
+allWords = sorted((set(allWords)))  # sort and remove all the duplicate words
+tags = sorted((set(tags)))  # removing and sort all the tag duplicates
 print(tags)
 
+aTrain = []  # bag of words
+bTrain = []  # associated number for each tags
 
-aTrain=[]#bag of words
-bTrain=[]#associated number for each tags
-
-#loop over our xy array
+# loop over our xy array
 for (tknize, tag) in xy:
-    #caliing from the nltk_down so here tknize is already tokenized
-    bag=bagOfWords(tknize, allWords)
-    #then append it into atrain array
+    # calling from the nltk_down so here tknize is already tokenized
+    bag = bagOfWords(tknize, allWords)
+    # then append it into train array
     aTrain.append(bag)
-    #numbers for our lables as in 0,1,2,....n
-    lable=tag.index(tag)
-    bTrain.append(lable)#CorssEntropyLoss that is why not doing 1 hot encoding
+    # numbers for our labels as in 0,1,2,....n
+    lable = tag.index(tag)
+    bTrain.append(lable)  # CorssEntropyLoss that is why not doing 1 hot encoding
 
-#after this we have to convert it into numpy array
-aTrain=np.array(aTrain)
-bTrain=np.array(bTrain)
+# after this we have to convert it into numpy array
+aTrain = np.array(aTrain)
+bTrain = np.array(bTrain)
 
-class CDataset(Dataset): #ChatDataset
+
+# PyTorch model and training
+
+class CDataset(Dataset):  # ChatDataset
     def __int__(self):
-        self.n_samples=len(aTrain)
-        self.adata=aTrain
-        self.bdata=bTrain
+        self.n_samples = len(aTrain) #store number of samples a train array
+        self.adata = aTrain
+        self.bdata = bTrain
 
-        #dataset[idx]
-        def __getitem__(self,index):
-            return self.adata[idx],self.bdata[idx]
+        # dataset[index]
+        def __getitem__(self, index): #
+            return self.adata[index], self.bdata[index]
+
+        def __len__(self):
+            return self.n_samples
+
+
+#hyperparameters
+batch_size=8;
+
+dataset=CDataset()
+# create a data loader here
+# batch size=8 (lets)
+# number of workers =2 --> it is for multiprocessing so that it works faster
+train_loader=DataLoader(dataset=CDataset, batch_size=8, shuffle=True , num_workers=2)
+# num_workers sometimes throws an error in windows so if that is the case just set it to 0
+# We can automatically iterate over it and can get a batch training
+
+# now let go for the training look for the Training.
+
