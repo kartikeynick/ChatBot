9dd0e01a5b8ee6f446750d4021e5783d3bd7898d
kartikeynick <47652496+kartikeynick@users.noreply.github.com>
2022-08-12 10:04:27 -0700

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,24 +1,25 @@
-/*class Chatbox {
+class Chatbox {
     constructor() {
+   
         this.args = {
             openButton: document.querySelector('.chatbox__button'),
             chatBox: document.querySelector('.chatbox__support'),
             sendButton: document.querySelector('.send__button')
         }
-        //state of our chatbot --> open or closed
-        this.state = false;
-        this.messages = [];// this is the array of messages
-    }
 
+        this.state = false;
+        this.messages = [];// message array to store the messages
+    }
+    // to display and start the even listeners
     display() {
-        const {openButton, chatBox, sendButton} = this.args;// extracting our arguments
-        //adding event listioner for recognising the clicks
+        const {openButton, chatBox, sendButton} = this.args;
+
         openButton.addEventListener('click', () => this.toggleState(chatBox))
 
         sendButton.addEventListener('click', () => this.onSendButton(chatBox))
 
         const node = chatBox.querySelector('input');
-        //Here if we just have to press enter to send the message
+        //to enable pressing enter to send the input in
         node.addEventListener("keyup", ({key}) => {
             if (key === "Enter") {
                 this.onSendButton(chatBox)
@@ -28,7 +29,8 @@
 
     toggleState(chatbox) {
         this.state = !this.state;
-        // show or hides the box
+
+        // show or hides the box 
         if(this.state) {
             chatbox.classList.add('chatbox--active')
         } else {
@@ -42,16 +44,16 @@
         if (text1 === "") {
             return;
         }
-        //we define a dictionary
+
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
-        window.print(msg1)
-        //We can hardcode out local hoste or use the script root function
+        
+        //We can hardcode out local hoste or use the script root function, this will be more useful in the actual dev environment 
         //fetch($SCRIPT_ROOT+'/predict',{
         fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
-            mode: 'cors', // if deployed a flask app seperately, this will be needed
+            mode: 'cors',
             headers: {
               'Content-Type': 'application/json'
             },
@@ -69,7 +71,7 @@
             textField.value = ''
           });
     }
-
+        // this will update the html file to encorporate the message in the suitable place
     updateChatText(chatbox) {
         var html = '';
         this.messages.slice().reverse().forEach(function(item, index) {
@@ -88,97 +90,6 @@
     }
 }
 
-
-const chatbox = new Chatbox();
-chatbox.display();*/
-class Chatbox {
-    constructor() {
-        this.args = {
-            openButton: document.querySelector('.chatbox__button'),
-            chatBox: document.querySelector('.chatbox__support'),
-            sendButton: document.querySelector('.send__button')
-        }
-
-        this.state = false;
-        this.messages = [];
-    }
-
-    display() {
-        const {openButton, chatBox, sendButton} = this.args;
-
-        openButton.addEventListener('click', () => this.toggleState(chatBox))
-
-        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
-
-        const node = chatBox.querySelector('input');
-        node.addEventListener("keyup", ({key}) => {
-            if (key === "Enter") {
-                this.onSendButton(chatBox)
-            }
-        })
-    }
-
-    toggleState(chatbox) {
-        this.state = !this.state;
-
-        // show or hides the box
-        if(this.state) {
-            chatbox.classList.add('chatbox--active')
-        } else {
-            chatbox.classList.remove('chatbox--active')
-        }
-    }
-
-    onSendButton(chatbox) {
-        var textField = chatbox.querySelector('input');
-        let text1 = textField.value
-        if (text1 === "") {
-            return;
-        }
-
-        let msg1 = { name: "User", message: text1 }
-        this.messages.push(msg1);
-
-        fetch('http://127.0.0.1:5000/predict', {
-            method: 'POST',
-            body: JSON.stringify({ message: text1 }),
-            mode: 'cors',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-          })
-          .then(r => r.json())
-          .then(r => {
-            let msg2 = { name: "Sam", message: r.answer };
-            this.messages.push(msg2);
-            this.updateChatText(chatbox)
-            textField.value = ''
-
-        }).catch((error) => {
-            console.error('Error:', error);
-            this.updateChatText(chatbox)
-            textField.value = ''
-          });
-    }
-
-    updateChatText(chatbox) {
-        var html = '';
-        this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Sam")
-            {
-                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
-            }
-            else
-            {
-                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
-            }
-          });
-
-        const chatmessage = chatbox.querySelector('.chatbox__messages');
-        chatmessage.innerHTML = html;
-    }
-}
-
-
+//envoking the progra,
 const chatbox = new Chatbox();
 chatbox.display();
